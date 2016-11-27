82fee9d83e10500cb8990fa7314e122047c5fd42
Milok Murlyka <milok.murlika@yandex.ru>
2016-11-28 01:27:59 +0200

    optimization

--- js/main.js
+++ js/main.js
@@ -8,7 +8,9 @@ $(document).ready(function() {
 		}
 	});
 
-	$(".main-slider__container").slick({
+	// Слайдеры начало
+
+	$(".js-main-slider").slick({
 		appendArrows: "#main-slider__arrows",
 		centerMode: true,
 		variableWidth: true,
@@ -25,12 +27,12 @@ $(document).ready(function() {
 		]
 	});
 
-	$(".aside-news__items").slick({
+	$(".js-aside-news-slider").slick({
 		infinite: false,
 		appendArrows: ".aside-news__title"
 	});
 
-	$(".affiche__items").slick({
+	$(".js-affiche-slider").slick({
 		dots: true,
 		slidesToShow: 4,
 		responsive: [
@@ -55,55 +57,25 @@ $(document).ready(function() {
 		]
 	});
 
-	$(".aside-social-tabs__list a").click(function() {
-
-		console.log($($(this).attr("href")).addClass("active"));
-
-		$(this).parents(3).find(".active").removeClass("active");
-
-		$(this).parent().addClass("active");
-		$($(this).attr("href")).addClass("active");
-
-
-		return false;
+	$(".js-article-slider").slick({
+		asNavFor: ".js-article-thumb"
 	});
 
-	$(".author__desc-link, .link-more").click(function() {
-
-		if($(this).data("open") == false) {
-			$(this).data("open", "true");
-			$(this).text("Скрыть");
-			$(this).prev().fadeIn();			
-		} else {
-			$(this).data("open", false);
-			$(this).text("Подробнее");
-			$(this).prev().fadeOut();
-		}
-
-		return false;
-	});
-
-	$(".js-collapse").collapse();
-
-	$(".article__slider").slick({
-		asNavFor: ".article__thumb"
-	});
-
-	$(".article__thumb").slick({
+	$(".js-article-thumb").slick({
 		slidesToShow: 4,
 		slidesToScroll: 4,
-		asNavFor: ".article__slider",
+		asNavFor: ".js-article-slider",
 		focusOnSelect: true
 	});
 
-	$(".articles-random__slider").slick({
+	$(".js-articles-random-slider").slick({
 		asNavFor: ".articles-random__thumb",
 		vertical: true,
 		verticalSwiping: true,
 		arrows: false
 	});
 
-	$(".articles-random__thumb").slick({
+	$(".js-articles-random-thumb").slick({
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		asNavFor: ".articles-random__slider",
@@ -121,9 +93,19 @@ $(document).ready(function() {
 		]
 	});
 
-	var menu = $(".dropdown-left").sliiide({
-		toggle: ".special-item", // the selector for the menu toggle, whatever clickable element you want to activate or deactivate the menu. A click listener will be added to this element.
-		exit_selector: ".rubric-detail__close, .rubric-briefly__close", // the selector for an exit button in the div if needed, when the exit element is clicked the menu will deactivate, suitable for an exit element inside the nav menu or the side bar
+	// Слайдеры конец
+
+
+
+	// колапсы
+	$(".js-collapse").collapse();
+
+	// Рубрики
+	
+
+	var menu = $(".js-dropdown").sliiide({
+		// toggle: ".special-item", // the selector for the menu toggle, whatever clickable element you want to activate or deactivate the menu. A click listener will be added to this element.
+		// exit_selector: ".rubric-detail__close, .rubric-briefly__close", // the selector for an exit button in the div if needed, when the exit element is clicked the menu will deactivate, suitable for an exit element inside the nav menu or the side bar
 		animation_duration: "0.5s", //how long it takes to slide the menu
 		place: "left", //where is the menu sliding from, possible options are (left | right | top | bottom)
 		// animation_curve: "cubic-bezier(0.54, 0.01, 0.57, 1.03)", //animation curve for the sliding animation
@@ -132,17 +114,14 @@ $(document).ready(function() {
 		auto_close: false //set to true if you want the slider to auto close everytime a child link of it is clicked
 	});
 
-	$(".special-item").click(function() {
+	$(".js-open-rubric").click(function() {
 		$("body, html").css("overflow", "hidden");
 		menu.activate();
 	});
 
-	// $(".special-item").bind("click", function() {
-	// 	$("body, html").css("overflow", "hidden");
-	// });
-
-	$(".rubric-detail__close, .rubric-briefly__close").bind("click", function() {
+	$(".js-close-rubric").click(function() {
 		$("body, html").css("overflow", "auto");
+		menu.deactivate();
 	});
 
 	$(".rubric-detail__tab:not(.active)").click(function() {
@@ -161,26 +140,37 @@ $(document).ready(function() {
 		return false;
 	});
 
-
-	$(".nav-foo__humburger").click(function() {
+	// меню
+	$(".js-open-menu").click(function() {
 		$(".nav-foo__list").toggleClass("nav-foo__list-open");
 
 		return false;
 	});
 
-	$(".header__humburger").click(function() {
-		$(".header__container").toggleClass("header__container-open");
+	// поле поиска
+	$(".js-header-open, .js-header-close").click(function() {
+		$(".js-header-inner").toggleClass("header__inner-open");
 
 		return false;
 	});
 
-	$(".header__search, .header__inner-close").click(function() {
-		$(".header__inner").toggleClass("header__inner-open");
-
-		return false;
+	//календарь
+	$('.responsive-calendar').responsiveCalendar({
+		translateMonths: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ]
 	});
 
-	$(".authors__tab").click(function () {
+
+	$('.responsive-calendar').responsiveCalendar('edit', {
+    "2016-11-30": {"url": "#"},
+    "2016-11-26": {},
+    "2013-06-12": {}
+  })
+
+	// стрелка вверх
+  $(".js-arrow-up").scrollUp();
+
+  // табы
+  $(".authors__tab").click(function () {
 		var container = $(this).data("container");
 
 		$(this).parent().find(".active").removeClass("active");
@@ -197,16 +187,41 @@ $(document).ready(function() {
 		return false;
 	});
 
-	$('.responsive-calendar').responsiveCalendar({
-		translateMonths: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ]
+	$(".js-filter-tab").click(function () {
+		var $container = $($(this).attr("href"));
+
+		$(this).parent().find(".active").removeClass("active");
+		$(this).addClass("active");
+
+		$(".js-filter-filtered .visible").toggleClass("hidden visible");
+		$container.toggleClass("hidden visible");
+
+		return false;
 	});
 
-	$('.responsive-calendar').responsiveCalendar('edit', {
-    "2016-11-30": {"url": "#"},
-    "2016-11-26": {},
-    "2016-11-30": {"number": 0, "badgeClass": "hidden", "url": "#"},
-    "2013-06-12": {}
-  })
 
-  $(".distribution__up").scrollUp();
+	$(".js-social-tab").click(function() {
+		$(this).parents(3).find(".active").removeClass("active");
+
+		$(this).parent().addClass("active");
+		$($(this).attr("href")).addClass("active");
+
+		return false;
+	});
+
+	// подробнее
+	$(".js-more").click(function() {
+
+		if($(this).data("open") == false) {
+			$(this).data("open", "true");
+			$(this).text("Скрыть");
+			$(this).prev().fadeIn();			
+		} else {
+			$(this).data("open", false);
+			$(this).text("Подробнее");
+			$(this).prev().fadeOut();
+		}
+
+		return false;
+	});
 });
